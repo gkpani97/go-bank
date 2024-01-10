05d31fc9cea62c9284adfe26e52846caa38cd90e
gkpani97 <gkpani97@gmail.com>
2024-01-10 10:55:24 +0530

    added a comment for the last commit, small update, but needs a seggregation from next commits

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,7 +15,7 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M){
 	// config, err := util.LoadConfig("../..") //in ubuntu change the slash
-	config, err := util.LoadConfig("../..")
+	config, err := util.LoadConfig("..\\..")
 	if err != nil {
 		log.Fatal("Cannot load configurations:", err)
 	}
