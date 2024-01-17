3e74505667164799d1ad12317938d03eb16b8cc5
gkpani97 <gkpani97@gmail.com>
2024-01-17 19:18:25 +0530

    added creatUser api

--- api/server.go
+++ api/server.go
@@ -22,10 +22,11 @@ func NewServer(store db.Store) *Server {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	router.POST("/users", server.createUser)
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
-	
+
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
