b189eb110799804f9c2f9043e3592ad39a1f972b
gkpani97 <gkpani97@gmail.com>
2024-03-09 23:55:18 +0530

    cautionary push to branch

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"runtime"
 	"testing"
 
 	"github.com/gkpani97/go-bank/util"
@@ -13,10 +14,20 @@ import (
 var TestQueries *Queries
 var testDB *sql.DB
 
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
+	var path string
 	// config, err := util.LoadConfig("../..") //in ubuntu change the slash
 	// config, err := util.LoadConfig("..\\..") // for windows
-	config, err := util.LoadConfig("../..")
+
+	os_type := runtime.GOOS
+
+	if os_type == "windows" {
+		path = "..\\.."
+	} else {
+		path = "../.."
+	}
+
+	config, err := util.LoadConfig(path)
 	if err != nil {
 		log.Fatal("Cannot load configurations:", err)
 	}
